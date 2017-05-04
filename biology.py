b34954b009cd16891a68db59623b924ce864a00d
Ank <leadnewindia@hotmail.com>
2017-05-04 14:10:19 +0530

    added a biology(main) file

---
+++ biology.py
@@ -0,0 +1,20 @@
+from z3 import *
+
+def dump(i,j):
+
+def node(i,k):
+
+def active_node(i,k):
+
+
+def real_edge(i,j):
+
+
+def edge(i,j,k):
+
+def active_edge(i,j,k):
+
+def pair_matrix(k,k1):
+
+
+
