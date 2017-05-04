72b0c1cba389ead7c5cec2d8971258d02508146e
Ashutosh Gupta <agupta@tifr.res.in>
2017-05-04 17:19:38 +0530

    init

--- biology.py
+++ biology.py
@@ -1,20 +1,59 @@
+from __future__ import print_function
 from z3 import *
+import argparse
+
+# input number of nodes and molecules
+
+parser = argparse.ArgumentParser(description='Auto testing for TARA')
+parser.add_argument("-M","--mols", type=int, default=3, help = "number of molecules")
+parser.add_argument("-N","--nodes", type=int, default=3, help = "number of nodes")
+args = parser.parse_args()
+M = args.mols
+N = args.nodes
+
+def printf(str, *args):
+    print(str % args, end='')
+
+
+printf( "Molecules : %d Nodes : %d\n", M, N )
+
 
 def dump(i,j):
+    s = "d_" + str(i) + "_" + str(j)
+    return Bool( s )
 
 def node(i,k):
+    s = "n_" + str(i) + "_k" + str(k)
+    return Bool( s )
 
 def active_node(i,k):
-
+    s = "a_" + str(i) + "_k" + str(k)
+    return Bool( s )
 
 def real_edge(i,j):
-
+    s = "e_" + str(i) + "_" + str(j)
+    return Bool( s )
 
 def edge(i,j,k):
+    s = "e_" + str(i) + "_" + str(j) + "_k" + str(k)
+    return Bool( s )
 
 def active_edge(i,j,k):
+    s = "b_" + str(i) + "_" + str(j) + "_k" + str(k)
+    return Bool( s )
 
 def pair_matrix(k,k1):
+    s = "p_k" + str(k) + "_k" + str(k1)
+    return Bool( s )
+
+# F_0
 
 
+# F_1
+
+# F_2
+
+# F_3
+
+solve(dump(0,2) or not dump(2,0))
 
