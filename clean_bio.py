0e85b8d51945a45bf2a2a42e5d3a8ae08582d671
Ank <ankitk@ncbs.res.in>
2017-05-10 23:26:54 +0530

    improved

--- clean_bio.py
+++ clean_bio.py
@@ -1,9 +1,7 @@
-# from __future__ import print_function
+#!/usr/bin/python
+
 from z3 import *
 import argparse
-
-# def printf(str, *args):
-#     print(str % args, end='')
 
 #----------------------------------------------------
 # input parsing
@@ -11,16 +9,13 @@ import argparse
 parser = argparse.ArgumentParser(description='Auto testing for TARA')
 parser.add_argument("-M","--mols", type=int, default=12, help = "number of molecules")
 parser.add_argument("-N","--nodes", type=int, default=3, help = "number of nodes")
-#parser.add_argument("-E","--edges", type=int, default=3, help = "number of edges")
 args = parser.parse_args()
 M = args.mols
 N = args.nodes
-#E = args.edges
-# printf( "Molecules : %d Nodes : %d\n", M, N )
-
 
 #----------------------------------------------------
 # Constraint generation 
+
 dump = [ [Bool ("d_{}_{}".format(i,j)) for j in range(N)] for i in range(N)]
 
 node = [ [Bool ("n_{}_{}".format(i,k)) for k in range(M)] for i in range(N)]
@@ -37,91 +32,87 @@ r = [ [ [Bool ("r_{}_{}_{}".format(i,j,k)) for k in range(M)] for j in range(N)]
 
 p = [ [Bool ("p_{}_{}".format(k,k1)) for k1 in range(M)] for k1 in range(M)]
 
-# Few additional condition that needs to be met.
+
+# Initial Constraints :
 
 #1. label(E) subset  label(N)
-# e_ijk -> aik
-# e_ijk -> ajk
-C0 = True
-C1 = True
-for i in range(N):
-    for j in range(N):
-        if i == j :
-            continue
-        for k in range(M):
-            C0 = And (Implies(presence_edge[i][j][k],node[i][k]),C1)
-            C1 = And (Implies(presence_edge[i][j][k],node[j][k]),C2)
+# e_ijk -> aik # e_ijk -> ajk
+
+C0 = [Implies(presence_edge[i][j][k],node[i][k]) for i in range(N) for j in range(N) for k in range(M)]
+C1 = [Implies(presence_edge[i][j][k],node[j][k]) for i in range(N) for j in range(N) for k in range(M)]
+
 
 #2. Self edges not allowed. 
 # not e_ii  
-for i in range(N):
-    C2 = Not(edge[i][i])
 
+C2 = [Not(edge[i][i]) for i in range(N)]
 
 #3. Multiple(parallel) edges are allowed between two nodes. 
 # But we restrict it to two.
+
 C3 = True
 
-#4. Condition on p_kk' : 
+#4. Condition on p_kk' : first and last cube is empty.
 # \/_{x<M/2,y>=M/2} !p(x,y) and !p(x,y)
-C4 = True
-for x in range(M):
-    for y in range(M):
-        if (x < M/2 and x <= y) or (x>M/2 and x>=y):
-            C4 = And( Not(p[x][y]),C4 )
 
-#5. Activitu on the node.
-C5 = True 
-for i in range(N):
-    for k in range(M):
-        C5 = And (Implies (active_node(i,k), node(i,k) ), C5) 
+C4 = [ Not(p[x][y])  for x in range(M) for y in range(M) if (x < (M/2) and x <= y) or (x > (M/2) and x>=y) ] 
+print C4
 
-# ----------------------------------------------------------------
+#5. Activity on the node.
+# if molecule is active on the node then its present. a(i,k) -> n(i,k) 
+
+C5 = [Implies (active_node[i][k], node[i][k]) for i in range(N) for k in range(M)] 
+
 
 # MAIN Constraints:
 
 # F_0
-# \/_k e_ijk -> e_ij  at evry edge there is an active molecules which is present.
-F0 = True, 
+# at evry edge there is an active molecules which is present. # \/_k e_ijk -> e_ij 
+
+F0 = [Implies(rhs, edge(i,j) ) for i in range(N) for j in range(N) if j != i else continue]
+
+
+F00 = True, 
 for i in range(N):
     for j in range(N):
         if j == i:
             continue        
         rhs = False
         for k in range(M):
-            rhs = Or( presence_edge[i][j][k], rhs )
-        F0 = Implies( rhs, edge[i][j] )
-# print F0
+            rhs = Or( presence_edge(i,j,k), rhs )
+        F00 = Implies(  rhs, edge(i,j) )
+# print F00
 
 
-# F1:  b_ijk -> e_ijk   
+# F_0:  b_ijk -> e_ijk   
 # If molecule is active on an edge then it should be present on the edge. \newline
-F1 = True
+
+F0 = True
 for i in range(N):
     for j in range(N):
         for k in range(M):
-            F1 = And( Implies(active_edge[i][j][k], presence_edge[i][j][k]), F1 )
-# print F1
+            F0 = And( Implies(active_edge(i,j,k), presence_edge(i,j,k)), F0 )
+# print F0
 
 
-# F_2
+# F_1
 # reachability condition; recursive definition. 
 # fixed point issues?? Check!
-F2 = True
+F11 = True
 for i in range(N):
     for j in range(N):
         if j == i:
             continue
         for k in range(M):
-            rhs = presence_edge[i][j][k]
+            rhs = edge(i,j,k)
             for l in range(N):
                 if i == l or j == l:
                     continue  
-                rhs = Or(And(r[l][j][k], presence_edge[i][l][k]), rhs)
-            F2 = And( Implies( r[i][j][k], rhs ), F2 )
-# print F2
+                rhs = Or(And(r(l,j,k), presence_edge(i,l,k)), rhs)
+            F11 = And( Implies( r(i,j,k), rhs ), F11 )
+# print F11
 
-F3 = True                
+F1 = True                
 # F_1_1
 # stability condition 
 # e_ijk -> r_jik
@@ -130,49 +121,49 @@ for i in range(N):
         if j == i:
             continue
         for k in range(M):
-           F3 = And( Implies(presence_edge[i][j][k], r[j][i][k]), F3)
+           F1 = And( Implies(presence_edge(i,j,k), r(j,i,k)), F1)
 
 
-# F4: Fusion rules:
+# F_2: Fusion rules:
 # /\_{i,j} ( \/_k e_{i,j,k}) -> \/_{k,k'} (b_{i,j,k} and a'_{j,k'} and p_{k,k'}) 
 # and /\_k b_{i,j,k} -> not(\/_{j' != j} (\/_k'' a'_{j',k''} and p_{k,k''}))  
-F4 = True
+F2 = True
 for i in range(N):
     for j in range(N):
         if i == j:
             continue
         rhs = False
         for k in range(M):
-            rhs = Or(presence_edge[i][j][k],rhs)        
+            rhs = Or(presence_edge(i,j,k),rhs)        
         for k1 in range(M): 
             for k2 in range(M):
-                lhs = And(active_edge[i][j][k1],active_node[i][k1],p[k1][k2]) 
-        F4 = And (Implies(rhs,lhs) , F4)
+                lhs = And(active_edge(i,j,k1),active_node(i,k1),p(k1,k2)) 
+        F2 = And (Implies(rhs,lhs) , F2)
 # print F2
 
-F5 = True
+F22 = True
 # for i in range(N):
 #     for j in range(N):
 #         if i == j:
 #             continue
 #         rhs = False
 #         for k in range(M):
-#             rhs = Or(active_edge[i][j][k],rhs)
+#             rhs = Or(active_edge(i,j,k),rhs)
 #             lhs = False
 #             for j1 in range(N):
 #                 if j == j1:
 #                     continue
 #                 for k11 in range(M):
-#                     lhs = Or(And (active_node[j1][k11],p[k][k11], lhs)
-#         F5 = And (Implies(rhs, Not(lhs)), F5)
+#                     lhs = Or(And (active_node(j1,k11),p(k,k11)), lhs)
+#         F22 = And (Implies(rhs, Not(lhs)), F22)
 
-Init = (presence_edge[0][1][0]) == True)
+Init = (edge(0,1,0) == True)
 
 # Create Solver and add constraints in it.
 s = Solver()
-s.add(C0,C1,C2,C3,C4,C5,F0,F1,F2,F3,F4,Init)
+s.add(C1,C2,C3,C4,F0,F00,F1,F11,F2,F22,Init)
 print "solving...\n"
-print s.check()
+print s.check b
 print "done\n"
 
 
@@ -182,18 +173,18 @@ def dump_dot( filename, m ) :
     for i in range(N):
         node_vec = ""
         for k in range(M):
-            if is_true(m[node[i][k]]) :
+            if is_true(m[node(i,k)]) :
                 node_vec = node_vec + "1"
             else:
                 node_vec = node_vec + "0"
-            if is_true(m.evaluate(active_node[i][k])) :
+            if is_true(m.evaluate(active_node(i,k))) :
                 node_vec = node_vec + "-"
         dfile.write( str(i) + "[label=\"" + node_vec + "\"]\n")
         for j in range(N):
             if i == j:
                 continue
             for k in range(M):
-                if is_true(m[presence_edge[i][j][k]]):                
+                if is_true(m[presence_edge(i,j,k)]):                
                     dfile.write( str(i) + "-> " + str(j) + "[label=" + str(k) +"]" +"\n" )
     dfile.write("}\n")
     for k2 in range(M):
@@ -201,7 +192,7 @@ def dump_dot( filename, m ) :
     print "\n"
     for k1 in range(M):
         for k2 in range(M):
-            if is_true(m[p[k1][k2]]):
+            if is_true(m[p(k1,k2)]):
                 print "\t0",
             else:
                 print "\t1",
