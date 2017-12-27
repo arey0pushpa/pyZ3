f2b9053f0f756758392eb32f03cfe200655fb537
Ankit <ankitk@ncbs.res.in>
2017-12-27 14:26:19 +0100

    initial-push

---
+++ vts-synth/build/buildr/CMakeFiles/vts-synth.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ankit/pyZ3/vts-synth/src/main.cpp" "/Users/ankit/pyZ3/vts-synth/build/buildr/CMakeFiles/vts-synth.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HB_UNICODE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/Users/ankit/pyZ3/vts-synth/src"
+  "/Users/ankit/pyZ3/vts-synth/src/library"
+  "../z3/src/api"
+  "../z3/src/api/c++"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ vts-synth/build/buildr/CMakeFiles/vts-synth.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vts-synth.dir/main.cpp.o"
+  "vts-synth.pdb"
+  "vts-synth"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vts-synth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
