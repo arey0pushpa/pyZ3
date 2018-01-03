b63bd4caa93bc849d8159fe7d6c593c9cfd87163
Ankit <ankitk@ncbs.res.in>
2018-01-03 17:47:26 +0100

    outline-qdimacs

--- vts-synth/src/main.cpp
+++ vts-synth/src/main.cpp
@@ -4,8 +4,8 @@
 
 #include "z3-util.h"
 
-int main() {
-
+int main(int argc, char* argv[]) {
+  try {
     z3::context c;
 
     z3::expr t = c.bool_val( true  );
@@ -15,38 +15,39 @@ int main() {
     z3::expr y = c.bool_const("y");
     z3::expr z = c.bool_const("z");
     z3::expr w = c.bool_const("w");
-    z3::expr f = exists( x, forall( z, x && z && forall( y, exists( w, implies( y, w) && x && z) ) ) );
-   // z3::expr f = forall(x, exists( y, (x == !y )) ) ;
-  //z3::expr f = forall( x, exists( w, w && forall ( y,  x&& y ) ) ) ;
+    z3::expr f = x && y;
+    //z3::expr f = exists( x, forall( z, x && z && forall( y, exists( w, implies( y, w) && x && z) ) ) );
+    //z3::expr f = forall(x, exists( y, !(x && y ) ) ) ;
+    //z3::expr f = forall( x, exists( w, w && forall ( y,  x&& y ) ) ) ;
+    //z3::expr f = forall( x, exists ( y,  forall (w, exists (z, x && y && w && z)  )) );
     // z3::expr f =  t;
    //z3::expr f = forall( x, forall ( y, exists (z,  z == x||  z == y) )) ; 
     //std::cout << "The sort of the formula f is: " << Z3_get_sort( c, f ) << "\n";
     //exit(0);
 
+//  std::cout << "At least this is working \n.";
+    //auto fml_f = negform ( c, f ); 
+    //negform ( c, f ); 
     std::vector<z3::expr_vector> qs;
     auto prenex_f = prenex( f, qs );
 
     std::cout << "Prenexed f : " << prenex_f << "\n";
 
-    bool fall = false;
-    for( auto& q : qs ) {
-      if( fall ) {
-        std::cout << "forall " << q << "\n";
-      }else{
-        std::cout << "exists " << q << "\n";
-      }
-      fall = !fall;
-    }
-
-    z3::solver s(c);
-    z3::goal g(c);
-    g.add( f );
-
-    z3::tactic t1(c, "qsat");
-    // // tactic t2(c, "solve-eqs");
-    // tactic t = t1 & t2;
-    z3::apply_result r = t1(g);
-    //std::cout << r << "\n";
-
+    //bool fall = false;
+    //for( auto& q : qs ) {
+    //  if( fall ) {
+    //    std::cout << "forall " << q << "\n";
+    //  }else{
+    //    std::cout << "exists " << q << "\n";
+     // }
+     // fall = !fall;
+   // }
+    
+  }
+  
+  catch (z3::exception & ex) {
+        std::cout << "Nuclear Missles are launched ----> " << ex << "\n";
+  }
+ 
   return 0;
 }
--- vts-synth/src/z3-util.h
+++ vts-synth/src/z3-util.h
@@ -1,12 +1,19 @@
 #ifndef VTS_Z3_UTIL
 #define VTS_Z3_UTIL
 
+#define _NO_OMP_
 #include <z3++.h>
 
 // function returns prenex body
 // and quantified variables in list of list qs.
 // The first entry in qs is for exists
 z3::expr prenex(z3::expr& fml, std::vector<z3::expr_vector>& qs );
+
+// Simplify the given z3 formula and convert it into nnf 
+//void negform ( z3::context& c, z3::expr& fml );
+
+// Simplify the given z3 formula and convert it into cnf 
+//z3::expr cnf-converter ( z3::context& c, z3::expr& fml, std::vector<z3::expr_vector>& qs );
 
 
 #endif
