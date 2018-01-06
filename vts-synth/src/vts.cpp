16e3456f7c2ded3fe9d87987189771aae9f5a213
Ashutosh Gupta <agupta@tifr.res.in>
2018-01-06 16:20:17 +0530

    popl fixed

--- vts-synth/src/vts.cpp
+++ vts-synth/src/vts.cpp
@@ -1,14 +1,16 @@
 #include<vts.h>
-// define n dimentional vectors 
+#include<z3-util.h>
+
+// define n dimentional vectors
 //#include "boost/multi_array.hpp"
 //#include <cassert>
 
 // Define the value of the num_nodes, total_molecules etc.
-#define n 2    // num of nodes.
-#define m 2    // num of molecules.
-#define q 1    // num of parellel edges.
-#define v 1    // model variation.
-#define c 2    // connectivity.
+// #define n 2    // num of nodes.
+// #define m 2    // num of molecules.
+// #define q 1    // num of parellel edges.
+// #define v 1    // model variation.
+// #define c 2    // connectivity.
 
 // Create a 3D array that is 3 x 4 x 2
 //typedef boost::multi_array<z3::expr, 3> array_type;
@@ -16,22 +18,26 @@
 //array_type A(boost::extents[3][4][2]);
 
 
-void popl1 ( VecExpr& m, unsigned arg1,
-                std::string& prefix) {
+void vts::popl1 ( VecExpr& m, unsigned arg1,
+                std::string& prefix ) {
   // Create a 1 dim vector m with dimentions arg1.
-  VecExpr m ( arg1 ); 
+  m.resize( arg1 );
   for ( unsigned int i = 0; i < arg1; i++ ) {
-    m[i] = prefix + "_" + to_string(arg1);
+    std::string name = prefix + "_" + std::to_string(arg1);
+    m[i] = make_bool( ctx, name );
   }
 }
-  
-void popl2 ( Vec2Expr& m, unsigned arg1, unsigned arg2, 
-                            std::string& prefix) {
-  
+
+void vts::popl2 ( Vec2Expr& m, unsigned arg1, unsigned arg2,
+                  std::string& prefix) {
+
+  m.resize(arg1);
+  for( auto& a : m) a.resize(arg2);
+
   // Create a 2 dim vector m  with dimentions arg1 arg2.
-  Vec2Expr m ( arg1, VectExpr ( arg2 ) ); 
-  
-  // Populate the vector. 
+  // Vec2Expr m ( arg1, VectExpr ( arg2 ) );
+
+  // Populate the vector.
   for ( unsigned int i = 0; i < arg1; i++ ) {
     for ( unsigned int j = 0; j < arg2; j++ ) {
         m[i][j]  = prefix + "_" + to_string(arg1) + "_" + to_string(arg2);
@@ -39,23 +45,29 @@ void popl2 ( Vec2Expr& m, unsigned arg1, unsigned arg2,
   }
 }
 
-void popl3 ( Vec3Expr& m, unsigned arg1, unsigned arg2, 
+void vts::popl3 ( Vec3Expr& m, unsigned arg1, unsigned arg2,
                             unsigned arg3, std::string& prefix) {
+  m.resize(arg1);
+  for( auto& a : m) {
+    a.resize(arg2);
+    for( auto& b : a) b.resize(arg3);
+  }
+
   // Create a 3 dim vector m  with dimention arg1 arg2 arg3.
-  Vec3Expr m ( arg1, Vec2Expr ( arg2, VecExpr (arg3) ) );
- 
-  // Populate the vector. 
+  // Vec3Expr m ( arg1, Vec2Expr ( arg2, VecExpr (arg3) ) );
+
+  // Populate the vector.
   for ( unsigned int i = 0; i < arg1; i++ ) {
     for ( unsigned int j = 0; j < arg2; j++ ) {
       for ( unsigned int k = 0; k < arg3; k++) {
-          m[i][j][j]  = prefix + "_" + to_string(arg1) + "_" + 
+          m[i][j][j]  = prefix + "_" + to_string(arg1) + "_" +
                               to_string(arg2) + "_" + to_string(arg3);
       }
     }
   }
 }
 
-void popl4 ( Vec3Expr& m, unsigned arg1, unsigned arg2, 
+void vts::popl4 ( Vec3Expr& m, unsigned arg1, unsigned arg2,
                             unsigned arg3, unsigned arg4,
                             std::string& prefix) {
   // Create a 4 dim vector m  with dimention arg1 arg2 arg3 arg4.
@@ -74,33 +86,33 @@ void popl4 ( Vec3Expr& m, unsigned arg1, unsigned arg2,
 void vts::init_vts() {
 
 // Populate edges: e(i,j,q)
-  edges = popl3 ( edges, n, n, q );
+  popl3( edges, N, N, E_arity, "e" );
 
 // Populate nodes: n(i,j)
-  nodes = popl2 ( nodes, n, m );
+  popl2 ( nodes, N, M, "n" );
 
 // Populate active_node (i, k)
-  active_node = popl2 ( active_node, n, m );
+  popl2 ( active_node, N, M, "a" );
 
 // Populate presence_edge(i,j,q,k)
-  presence_edge = popl4 ( presence_edge, n, n, q, m );
+  popl4 ( presence_edge, N, N, E_arity, M, "e" );
 
 // Populate active_edge(i,j,q,k)
-  active_edge = popl4 ( active_edge, n, n, q, m );
-  
+  popl4 ( active_edge, N, N, E_arity, M, "b" );
+
 // Populate pairing_m(k,k1)
-  pairing_m = popl2 ( pairing_m, m, m );
+  popl2 ( pairing_m, M, M, "p" );
 
 // Populate node_funcs : Currently not handled.
 // Populate edge_funcs : Currently not handled.
 
 // Populate reach(i,j,k,z)
-  reach = popl4 ( n, n, m, n-1 );
+  popl4 ( reach, N, N, M, N-1, "r" );
 
 // Populate drop1(i,j,q)
-  drop = popl3 ( n, n, q );
+  popl3 ( drop1, N, N, E_arity, "d1");
 
 //Populate d_reach(i,j)
-  d_reach = popl2 ( n , n ); 
+  popl2 ( d_reach, N , N, "r1" );
 
-}  
+}
--- vts-synth/src/vts.h
+++ vts-synth/src/vts.h
@@ -5,7 +5,7 @@
 #include <z3++.h>
 #include <z3-util.h>
 
-enum model_type {
+enum model_version {
   MODEL_1,
   MODEL_2,
   MODEL_3,
@@ -18,8 +18,9 @@ enum model_type {
 class vts{
 public:
   vts( z3::context& _ctx,
-       unsigned _M, unsigned _N, unsigned _Q, model_type _V, unsigned _C )
-    : ctx(_ctx), mols(_M), nodes(_N), e_arity(_Q), mod(_V), conn(_C)
+       unsigned _M, unsigned _N, unsigned _Q, model_version _V, unsigned _C )
+    : ctx(_ctx), M(_M), N(_N), E_arity(_Q),
+      V(_V), C(_C)
   {
     init_vts();
   }
@@ -27,17 +28,17 @@ public:
 private:
   z3::context& ctx;
 
-  unsigned   m;    // Molecules
-  unsigned   n;    // Nodes
-  unsigned   q;    // Edge arity
-  model_type v;  // Variation
-  unsigned   c;    // connectedness
+  unsigned   M;    // Molecules
+  unsigned   N;    // Nodes
+  unsigned   E_arity;    // Edge arity
+  model_version V;  // Variation
+  unsigned   C;    // connectedness
 
   void init_vts();
 
   //variables
   Vec3Expr edges;
-  Vec2Expr node;
+  Vec2Expr nodes;
   Vec2Expr active_node;
   Vec4Expr presence_edge;
   Vec4Expr active_edge;
@@ -58,7 +59,10 @@ private:
 
   //constraints
 
-
+  void popl1( VecExpr&, unsigned,  std::string&);
+  void popl2( Vec2Expr&, unsigned, unsigned, std::string&);
+  void popl3( Vec3Expr&, unsigned, unsigned, unsigned, std::string&);
+  void popl4( Vec4Expr&, unsigned, unsigned, unsigned, unsigned, std::string&);
 };
 
 #endif
