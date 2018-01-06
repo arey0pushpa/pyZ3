a9c9639bc025b19cb77d551009a803ab2cd0cf28
Ashutosh Gupta <agupta@tifr.res.in>
2018-01-06 23:57:56 +0530

    small fix

--- vts-synth/src/vts.cpp
+++ vts-synth/src/vts.cpp
@@ -315,11 +315,11 @@ z3::model vts::get_vts_for_prob1( ) {
   z3::expr r1 = reachability_def();           //R1
   z3::expr r2 = study_state_stability_cond(); //R2
 
-  z3::expr cons = v1 && v2 && v3 & v4 && v5 && v6 && v7 && v8 && r1 && r2;
+  z3::expr cons = v1 && v2 && v3 && v4 && v5 && v6 && v7 && v8 && r1 && r2;
 
   z3::solver s(ctx);
   s.add( cons );
-  if( s.check() == z3::unsat ) {
+  if( s.check() == z3::sat ) {
     z3::model m = s.get_model();
     return m;
   }else{
