ceb0c6094bc7ca99d455b7c2f4723c937510fafc
Ankit <ankitk@ncbs.res.in>
2018-04-13 12:29:12 +0200

    push

--- vts-synth/src/vts-synthesis.cpp
+++ vts-synth/src/vts-synthesis.cpp
@@ -19,11 +19,7 @@ void create_edges () {
 
 }
 
-void load_var (z3::expr_vector& l, z3::expr ) {
-
-}
-
-z3::expr vts::annotate_mukund_graph ( z3::expr_vector& fixN, z3::expr_vector& fixActiveN, z3::expr_vector& fixE, z3::expr_vector& fixPresenceE, z3::expr_vector& fixActiveE, z3::expr_vector& fixPairingP )  {
+z3::expr vts::annotate_mukund_graph () {
 
   /* M = 14, Molecules subgraph of Mukunds VTS`
    * [ Qa2, Qa5, Qa7, Qa8 ] ::> [ M0, M1, M2, M3 ]
@@ -38,81 +34,84 @@ z3::expr vts::annotate_mukund_graph ( z3::expr_vector& fixN, z3::expr_vector& fi
    *        PM :: 2
    */
 
-  /*
-  // Straighten out few things.
-  z3::expr_vector setE = edge_set();
-  z3::expr_vector setN = node_set();
-  z3::expr_vector setActiveN = active_node_set();
-  z3::expr_vector setPresenceE = presence_edge_set();
-  z3::expr_vector setActiveE = active_edge_set();
-  z3::expr_vector setPairingM = pairing_m_set();
-  z3::expr_vector setReach = reach_set();
-  */
-
   // Fix nodes of the graph
+  z3::expr n0_cons = nodes[0][0]
+                   && nodes[0][8];
 
-  auto n0 = nodes[0][0];
-  auto n1 = nodes[0][8];
+  z3::expr n1_cons = nodes[1][3]
+                   && nodes[1][4]
+                   && nodes[1][6];
 
-  auto n2 = nodes[1][3];
-  auto n3 = nodes[1][4];
-  auto n4 = nodes[1][6];
-  
-  auto n5 = nodes[2][1];
-  auto n6 = nodes[2][2];
-  auto n7 = nodes[2][7];
-  auto n8 = nodes[2][9];
+  z3::expr n2_cons = nodes[2][1]
+                   && nodes[2][2]
+                   && nodes[2][7]
+                   && nodes[2][9];
 
-  fixN.push_back(n0);
-  fixN.push_back(n1);
-  fixN.push_back(n2);
-  fixN.push_back(n3);
-  fixN.push_back(n4);
-  fixN.push_back(n5);
-  fixN.push_back(n6);
-  fixN.push_back(n7);
-  fixN.push_back(n8);
-
-  z3::expr nodes_cons = z3::mk_and( fixN ); 
+  auto nodes_cons = n0_cons && n1_cons && n2_cons; 
 
   // Fix edges of the graph 
-  auto e0 = edges[0][1][0]; 
-  auto e1 = edges[1][2][0]; 
-  auto e2 = edges[2][0][0];
-  auto e3 = edges[0][2][0];
-  
-  fixE.push_back(e0);
-  fixE.push_back(e1);
-  fixE.push_back(e2);
-  fixE.push_back(e3);
-
-  z3::expr edge_cons = z3::mk_and ( fixE );
+  z3::expr e_cons = edges[0][1][0] 
+                  && edges[1][2][0] 
+                  && edges[2][0][0] 
+                  && edges[2][0][1];
 
   // Fix edge labels of the graph 
-  e0 = presence_edge[0][1][0][2];
-  e1 = presence_edge[0][1][0][9];   
-  e2 = presence_edge[0][1][0][12];    
-  e3 = presence_edge[0][1][0][13];    
-
-  auto e4 = presence_edge[0][2][0][11];    
-
-  auto e5 = presence_edge[1][2][0][9];    
+  z3::expr e_label_01 = presence_edge[0][1][0][2]    
+                      && presence_edge[0][1][0][9]    
+                      && presence_edge[0][1][0][12]    
+                      && presence_edge[0][1][0][13];    
   
-  auto e6 = presence_edge[2][0][0][0];
-  auto e7 = presence_edge[2][0][0][2];
-  auto e8 = presence_edge[2][0][0][9];   
-  auto e9 = presence_edge[2][0][0][10];    
-  auto e10 = presence_edge[2][0][0][12];    
+  z3::expr e_label_02 = presence_edge[2][0][0][11];    
+
+  z3::expr e_label_12 = presence_edge[1][2][0][9];    
   
-  fixPresenceE.push_back(e0);
-  fixPresenceE.push_back(e1);
-  fixPresenceE.push_back(e2);
-  fixPresenceE.push_back(e3);
-  fixPresenceE.push_back(e4);
-  fixPresenceE.push_back(e5);
-  fixPresenceE.push_back(e6);
-  fixPresenceE.push_back(e7);
-  fixPresenceE.push_back(e8);
+  z3::expr e_label_20 = presence_edge[2][0][0][0]   
+                        && presence_edge[2][0][0][2]    
+                        && presence_edge[2][0][0][9]    
+                        && presence_edge[2][0][0][10]    
+                        && presence_edge[2][0][0][12];    
+
+
+  auto edgel_cons = e_label_01 && e_label_02 && e_label_12 && e_label_20;
+
+  auto cons = nodes_cons && e_cons && edgel_cons;
+  return cons;
+
+  /* 21 Molecules subgraph of Mukunds VTS' 
+   * [ Qa2, Qa4, Qa5, Qa6, Qa7 ] ::> [ M0, M1, M2, M3, M4 ]
+   * [ Qb1, Qb6 ] ::> [ M5, M6 ]
+   * [ Qc4, Qc5 ] ::> [ M7, M8 ]
+   * [ Qbc2, Qbc3, Qbc7 ] ::> [ M9, M10, M11 ] 
+   * [ Qbc2, Qbc2/3, Qbc7 ] ::> [ M12, M13, M14 ]
+   * [ R2, R3, R4, R6, R7, R8 ] ::> [ M15, M16, M17, M18, M19, M20 ]
+   * */
+
+}
+
+z3::expr vts::annotate_plos_graph () {
+  /** Example taken from PLOS paper
+   * N = 2, M = 6 
+   * model 3: Arb on edge, Nothing on node
+   */
+
+  /** node: n[i][k] : below.  a[i][k] : use Model_3 **/
+  // Node 0 : [111 110]  
+  z3::expr node_cons = !nodes[0][0] && nodes[0][1] && nodes[0][2] && nodes[0][3] && nodes[0][4] && nodes[0][5]; 
+
+  /** edge: e[i][j][q], e[i][j][q][k]: below, b[i][j][q][k]: Model_3 **/
+  //auto edge_cons1 = edges[0][1][0] && edges[1][0][0] && edges[1][0][1];
+  z3::expr edge_cons1 = edges[0][1][0] && edges[1][0][0];
+  z3::expr edge_cons2_1 = presence_edge[0][1][0][2] && presence_edge[0][1][0][3] && presence_edge[0][1][0][5]; 
+  z3::expr edge_cons2_2 = presence_edge[1][0][0][2] && presence_edge[1][0][0][3];
+  z3::expr edge_cons = edge_cons1 && edge_cons2_1 && edge_cons2_2; 
+
+  /** Pairing Matrix Constraints **/
+  z3::expr pairing_cons_1 = pairing_m[5][1] && pairing_m[3][0] && pairing_m[2][4]; 
+
+  /*
+     std::vector<z3::expr> vec;
+     vec.push_back( pairing_m[2][4] ); 
+     vec.push_back( pairing_m[3][0] ); 
   fixPresenceE.push_back(e9);
   fixPresenceE.push_back(e10);
 
@@ -223,53 +222,8 @@ z3::expr vts::vts_synthesis ( unsigned variation ) {
   z3::expr_vector setPairingM = pairing_m_set(); 
   z3::expr_vector setReach = reach_set();
 
+}
             
-  unassigned_bits ( setN, fixN, openN ); 
-  unassigned_bits ( setActiveN, fixActiveN, openActiveN ); 
-  unassigned_bits ( setE, fixE, openE ); 
-  unassigned_bits ( setPresenceE, fixPresenceE, openPresenceE ); 
-  unassigned_bits ( setActiveE, fixActiveE, openActiveE ); 
-
-  // 1. Add edge to achieve graph stability and k connected. 
-  if ( variation == 1 ) {
-    
-    auto nodeC = ! at_least_four ( openN );
-    auto nodeActivityC = ! at_least_two ( openActiveE );
-    auto edgeC = ! at_least_three ( openE );
-    auto edgeActivityC = ! at_least_three ( openActiveE );
-    auto edgePresenceC = ! at_least_three ( openPresenceE );
-
-    auto qvtsCons = exists( setN, 
-                    exists( setActiveN, 
-                    exists( setPresenceE,  
-                    exists( setActiveE, 
-                    exists( setPairingM, 
-                    exists( setReach, 
-                            vtsCons && vtsActivity && inputCons ))))));   
-
-    auto cons = exists( setE, 
-                        qvtsCons && V5 && kConnCons ); 
-
-    return cons;
-
-  }
-
-  // 2. Add flow of molecules to fix fusion. 
-  else if ( variation == 2 )  {
-
-    z3::expr qvtsCons = exists( setN, 
-                        exists( setActiveN, 
-                        exists( setActiveE, 
-                        exists( setPairingM, 
-                        exists( setReach, 
-                                vtsCons && vtsActivity )))));
-
-    auto cons = exists( setPresenceE, 
-                exists( setE, 
-                        qvtsCons && V5 && inputCons && kConnCons )); 
-
-    return cons;
-  }
   // 3. KCnf  4. Cnf with low depth circuit
   else if ( variation == 3 ) {
 
@@ -285,7 +239,7 @@ z3::expr vts::vts_synthesis ( unsigned variation ) {
 
     z3::expr func3cnf  = exists( setN, 
                          exists( setActiveN, 
-                         exists( setPresenceE, 
+                         exists( setPresentE, 
                          exists( setActiveE, 
                          exists( setPairingM, 
                          exists( setReach, 
@@ -318,7 +272,7 @@ z3::expr vts::vts_synthesis ( unsigned variation ) {
     
     z3::expr funcGate  = exists( setN, 
                          exists( setActiveN, 
-                         exists( setPresenceE, 
+                         exists( setPresentE, 
                          exists( setActiveE, 
                          exists( setPairingM, 
                          exists( setReach, 
@@ -327,7 +281,7 @@ z3::expr vts::vts_synthesis ( unsigned variation ) {
     z3::expr cons = exists( setSvar, 
                     exists( setTvar, 
                     exists( setUvar, 
-                    exists( setVvar, 
+                    exists( setUvar, 
                     exists( setE, 
                             kConnCons && V5 && inputCons && funcGate )))));
 
