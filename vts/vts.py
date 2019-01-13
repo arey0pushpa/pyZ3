753316d80c894d084b1c546fe13ac4bde208d957
ankit <ankitk@ncbs.res.in>
2019-01-13 04:31:13 +0100

    Table update

--- vts/vts.py
+++ vts/vts.py
@@ -403,7 +403,9 @@ for i in range(N):
                 rhs = Or( Or (And( edge[i][l][q], Not (dump1[i][l][q]) ), And (edge[l][i][q], Not ( dump1[l][i][q]) ) ) , rhs)
             #rhs = And( rhs, r1[l][j])
             kbc = Or ( kbc, And (rhs, r1[i][j])) 
-        w = Implies( r1[i][j], Or (bhs , kbc) )
+        # THIS WAS A BUG :)
+        #w = Implies( r1[i][j], Or (bhs , kbc) )
+        w = Implies( Or (bhs , kbc), r1[i][j] )
         A_list.append(w)
 D3_reachability = And(A_list)
 
